d4df017b998d8201748d8204a0779cba15dbe876
Marcos Martini <marcosrmartini@gmail.com>
2017-11-20 06:12:33 -0200

    Inclusão de Ajax

--- src/main/resources/static/js/GerarTAG.js
+++ src/main/resources/static/js/GerarTAG.js
@@ -1,16 +1,30 @@
-//jQuery(document).ready(function(){
-//	jQuery('#frmUserTCTB').submit(function(){
-//		var dados = jQuery( this ).serialize();
-//
-//		jQuery.ajax({
-//			type: "POST",
-//			url: "/tag/cadastro/customer/userTrackingConfigurationTriggerButton",
-//			data: dados,
-//			success: function( data )
-//			{
-//				alert( data );
-//			}
-//		});
-//		return false;
-//	});
-//});
+function setarForm(formulario, url) {
+	jQuery(document).ready(function(){
+		jQuery(formulario).submit(function(){
+			var dados = jQuery( this ).serialize();
+			
+			$.blockUI(); //Bloquear UI
+	
+			jQuery.ajax({
+				type: "POST",
+				url: url,
+				data: dados,
+				success: function( data ) {
+	//				alert( data );
+					$.unblockUI(); //Desbloquear UI
+					setTimeout(
+			                  function() {
+			                     location.reload();
+			                  }, 0001); 
+				}
+			});
+			return false;
+		});
+	});
+};
+
+setarForm("#frmUrls", "/tag/cadastro/customer/url");
+setarForm("#frmTrackedHtmlControl", "/tag/cadastro/customer/trackedhtmlcontrol");
+setarForm("#frmModalContent", "/tag/cadastro/customer/modalcontent");
+setarForm("#frmUserTrackingConfiguration", 	"/tag/cadastro/customer/userTrackingConfiguration");
+setarForm("#frmUserTCTB", "/tag/cadastro/customer/userTrackingConfigurationTriggerButton");
